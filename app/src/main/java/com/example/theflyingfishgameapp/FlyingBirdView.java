1bbbae5d719d7ffb4c48e6e6d1a277dae29ca831
Prakhar Jain <prakhar.jain1810@gmail.com>
2021-06-07 18:17:44 +0530

    TheFlyingBirdApp

---
+++ app/src/main/java/com/example/theflyingfishgameapp/FlyingBirdView.java
@@ -0,0 +1,204 @@
+package com.example.theflyingfishgameapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class FlyingBirdView extends View
+{
+    private Bitmap bird[] = new Bitmap[2];
+    private int birdX = 10;
+    private int birdY;
+    private int birdSpeed;
+
+    private int canvasWidth, canvasHeight;
+
+    private int yellowX, yellowY, yellowSpeed = 16;
+    private Paint yellowPaint = new Paint();
+
+    private int greenX, greenY, greenSpeed = 20;
+    private Paint greenPaint = new Paint();
+
+    private int redX, redY, redSpeed = 25;
+    private Paint redPaint = new Paint();
+
+    private int score, lifeCounterOfBird;
+
+    private boolean touch = false;
+
+    private Bitmap backgroundImage;
+
+    private Paint scorePaint = new Paint();
+
+    private Bitmap life[] = new Bitmap[2];
+
+    public FlyingBirdView(Context context)
+    {
+        super(context);
+
+        bird[0] = BitmapFactory.decodeResource(getResources(), R.drawable.birdy1);
+        bird[1] = BitmapFactory.decodeResource(getResources(), R.drawable.birdy2);
+
+        backgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.cloudy4);
+
+        yellowPaint.setColor(Color.YELLOW);
+        yellowPaint.setAntiAlias(false);
+
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setAntiAlias(false);
+
+        redPaint.setColor(Color.RED);
+        redPaint.setAntiAlias(false);
+
+
+        scorePaint.setColor(Color.WHITE);
+        scorePaint.setTextSize(70);
+        scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
+        scorePaint.setAntiAlias(true);
+
+        life[0] = BitmapFactory.decodeResource(getResources(), R.drawable.hearts);
+        life[1] = BitmapFactory.decodeResource(getResources(), R.drawable.heart_grey);
+
+        birdY = 550;
+        score = 0;
+        lifeCounterOfBird = 3;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        canvasWidth = canvas.getWidth();
+        canvasHeight = canvas.getHeight();
+
+        canvas.drawBitmap(backgroundImage, 0, 0, null);
+
+        int minbirdY = bird[0].getHeight();
+        int maxbirdY = canvasHeight - bird[0].getHeight() * 3;
+        birdY = birdY + birdSpeed;
+
+        if(birdY < minbirdY)
+        {
+          birdY = minbirdY;
+        }
+        if(birdY > maxbirdY)
+        {
+            birdY = maxbirdY;
+        }
+        birdSpeed = birdSpeed + 2;
+
+        if(touch)
+        {
+          canvas.drawBitmap(bird[1], birdX, birdY, null);
+          touch = false;
+        }
+        else{
+            canvas.drawBitmap(bird[0], birdX, birdY, null);
+        }
+
+        yellowX = yellowX - yellowSpeed;
+
+        if (hitballChecker(yellowX, yellowY))
+        {
+            score = score + 10;
+            yellowX = -100;
+        }
+
+        if(yellowX < 0)
+        {
+            yellowX = canvasWidth + 21;
+            yellowY = (int) Math.floor(Math.random() * (maxbirdY-minbirdY)) + minbirdY;
+        }
+        canvas.drawCircle(yellowX, yellowY, 25, yellowPaint);
+
+
+        greenX = greenX - greenSpeed;
+
+        if (hitballChecker(greenX, greenY))
+        {
+            score = score + 20;
+            greenX = -100;
+        }
+
+        if(greenX < 0)
+        {
+            greenX = canvasWidth + 21;
+            greenY = (int) Math.floor(Math.random() * (maxbirdY-minbirdY)) + minbirdY;
+        }
+        canvas.drawCircle(greenX, greenY, 25, greenPaint);
+
+
+        redX = redX - redSpeed;
+
+        if (hitballChecker(redX, redY))
+        {
+            redX = -100;
+            lifeCounterOfBird--;
+
+            if (lifeCounterOfBird == 0)
+            {
+                Toast.makeText(getContext(), "Game Over", Toast.LENGTH_SHORT).show();
+
+                Intent gameoverIntent = new Intent(getContext(), GameOverActivity.class);
+                gameoverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameoverIntent.putExtra("score", score);
+                getContext().startActivity(gameoverIntent);
+            }
+        }
+
+        if(redX < 0)
+        {
+            redX = canvasWidth + 21;
+            redY = (int) Math.floor(Math.random() * (maxbirdY-minbirdY)) + minbirdY;
+        }
+        canvas.drawCircle(redX, redY, 30, redPaint);
+
+
+        canvas.drawText("Score : " + score, 20, 60, scorePaint);
+
+
+        for (int i=0; i<3; i++)
+        {
+            int x = (int) (580 + life[0].getWidth() * 1.5 * i);
+            int y = 30;
+
+            if(i< lifeCounterOfBird)
+            {
+                canvas.drawBitmap(life[0] , x, y, null);
+            }
+            else
+            {
+                canvas.drawBitmap(life[1] , x, y, null);
+            }
+        }
+    }
+
+    public boolean hitballChecker(int x, int y)
+    {
+        if (birdX < x && x < (birdX + bird[0].getWidth())  &&  birdY < y && y < (birdY + bird[0].getHeight()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            touch = true;
+
+            birdSpeed = -24;
+        }
+        return true;
+    }
+}
